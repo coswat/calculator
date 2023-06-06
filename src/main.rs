798e405e8bfd7bfc99703b868eed97d2898655e1
coderswat <coderswat@gmail.com>
2023-06-07 02:34:07 +0530

    updated calculator

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ fn run_calculater() {
     let mut opr: String = String::new();
     io::stdin().read_line(&mut first);
     // Parse the first number
-    let first: i32 = match first.trim().parse() {
+    let first: f32 = match first.trim().parse() {
         Ok(num) => num,
         Err(_) => {
             println!(
@@ -51,7 +51,7 @@ fn run_calculater() {
     println!("{}Enter Second Number :{}", "\x1b[32m", "\x1b[0m");
     io::stdin().read_line(&mut second);
     // Parse the second number
-    let second: i32 = match second.trim().parse() {
+    let second: f32 = match second.trim().parse() {
         Ok(num) => num,
         Err(_) => {
             println!(
@@ -67,52 +67,46 @@ fn run_calculater() {
     );
     io::stdin().read_line(&mut opr);
     // Check the validity of numbers and perform the calculation
-    if first > i32::MAX {
+    if first > f32::MAX {
         println!(
             "{}First number must be between 1 - {} , {} given{}",
             "\x1b[31m",
-            i32::MAX,
+            f32::MAX,
             first,
             "\x1b[0m"
         );
-    } else if second > i32::MAX {
+    } else if second > f32::MAX {
         println!(
             "{}Second number must be between 1 - {} , {} given{}",
             "\x1b[31m",
-            i32::MAX,
+            f32::MAX,
             second,
             "\x1b[0m"
         );
     } else {
-        calculate_number(&opr, first, second);
+        let ans: f32 = calculate_number(&opr, first, second);
+        println!("{} {} {} = {}", first, opr, second, ans);
     }
 }
 /// Calculates the result based on the operator and prints it.
 /// Supports addition (+), subtraction (-), multiplication (*), and division (/).
-fn calculate_number(opr: &str, first: i32, second: i32) {
+fn calculate_number(opr: &str, first: f32, second: f32) -> f32 {
     match opr.trim() {
         // Addition operatiom
-        "+" => println!("Addition {} + {} = {}", first, second, first + second),
+        "+" => first + second,
         // Substraction operatiom
-        "-" => println!("Substraction {} - {} = {}", first, second, first - second),
+        "-" => first - second,
         // Multiplication operatiom
-        "*" => println!("Multiplication {} * {} = {}", first, second, first * second),
+        "*" => first * second,
         // Division operatiom
         "/" => {
             // check the second number is 0
-            if second == 0 {
-                println!("{}Number cant be divisble by 0{}", "\x1b[31m", "\x1b[0m");
-                return;
+            if second == 0.0 {
+                return 0.0;
             }
-            println!("Division {} / {} = {}", first, second, first / second);
+            first / second
         }
-        // Handle th error for invalid operater
-        _ => {
-            println!(
-                "{}Invalid operater {} , supported [/, *, +, -]{}",
-                "\x1b[31m", opr, "\x1b[0m"
-            );
-            return;
-        }
-    };
+        // Handle th error for invalid operater*/
+        _ => return 0.0,
+    }
 }
